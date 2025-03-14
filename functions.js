827fd80aab3d002818b07d5c5480b638183439c5
nithu270 <127609533+nithu270@users.noreply.github.com>
2025-03-14 12:25:33 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,20 @@
+//if else is basically similar to all language
+
+// loops
+// for and while similar to all lang
+
+//functions
+
+
+//FUNCTION DECLARATION () -defining a function
+
+function square(num){  //js has a reserved keyword "function" to create function
+    return num*num;
+}
+
+//you should not create function like this 
+
+// function var(num){ 
+"var is a reserved keyword so we should not use it to create a function name"
+// }
+
---
+++ operatorsandequality.js
@@ -0,0 +1,19 @@
+//Is equal
+
+const a = 5;
+const b = 6;
+console.log(a==b); //f
+
+console.log(5=="5"); //t\\  // this both are actually not equal
+// but this is loose equality so it returns true
+
+//strict equality and strict inequality
+//comapres both type and value
+console.log(5==="5"); //f\\ 
+
+//logical operators
+
+
+
+
+
