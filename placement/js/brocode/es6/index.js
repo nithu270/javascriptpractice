8a009c5cdde456a4e438fe042d9a008129620c49
nithu270 <nithyashree.it22@bitsathy.ac.in>
2025-06-08 23:27:47 +0530

    added

---
+++ placement/js/brocode/es6/index.js
@@ -0,0 +1,4 @@
+import {pi,radiusofcircle,sum} from './math.js';
+let a = 10;
+let b = 20;
+console.log(sum(a,b));
---
+++ placement/js/brocode/es6/math.js
@@ -0,0 +1,7 @@
+export const pi = 3.14;
+export function radiusofcircle(pi,r){
+    return pi*r*r;
+}
+export function sum(a,b){
+    return a+b;
+}
